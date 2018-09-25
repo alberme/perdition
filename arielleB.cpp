179407e3661c26fd4794c824d1fc9662a23bd5a2
ArieBattle <42813810+ArieBattle@users.noreply.github.com>
2018-09-25 12:02:42 -0700

    Add files via upload
    new

--- arielleB.cpp
+++ arielleB.cpp
@@ -8,8 +8,8 @@
 void ShowArielleName(int x, int y)
 {
     	Rect r;
-	r.bot = y ;
-	r.left = x;
+	r.bot = y - 10 ;
+	r.left = x + 100;
 	r.center = 0;
 	ggprint8b(&r, 16, 0x00ffff44, "Arielle Battle");
 }
