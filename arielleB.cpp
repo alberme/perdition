9bc6cebe41ea92b1e036b193b921b173f521801b
ArieBattle <42813810+ArieBattle@users.noreply.github.com>
2018-09-25 12:11:36 -0700

    Update arielleB.cpp

--- arielleB.cpp
+++ arielleB.cpp
@@ -8,8 +8,8 @@
 void ShowArielleName(int x, int y)
 {
     	Rect r;
-	r.bot = y - 10 ;
-	r.left = x + 300;
+	r.bot = y - 160 ;
+	r.left = x + 210;
 	r.center = 0;
 	ggprint8b(&r, 16, 0x00ffff44, "Arielle Battle");
 }
