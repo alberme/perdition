4853cdc098c08c8514a2845f26df138355d7ccb9
FranciscoU <42758867+FranciscoU@users.noreply.github.com>
2018-09-25 11:45:12 -0700

    Add files via upload
    fixed a few things

--- franciscoU.cpp
+++ franciscoU.cpp
@@ -4,10 +4,8 @@
 extern void showFranciscoName(int x, int y)
 {
     Rect r;
-    unsigned int c = xffff44;
-    unsinged int c = x
-    r.bot = gl.yres -20;
+    r.bot = y - 20;
     r.left = 10;
-    r.center -0;
-    ggprint8b(&r, 16, c, "Francisco U");
+    r.center = 0;
+    ggprint8b(&r, 16, 0x00ffff44, "Francisco U");
 }
