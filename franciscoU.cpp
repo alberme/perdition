64c8cff9f96b5add5ab0840d3ca3f8acde330bd0
FranciscoU <42758867+FranciscoU@users.noreply.github.com>
2018-09-25 12:20:52 -0700

    Update franciscoU.cpp

--- franciscoU.cpp
+++ franciscoU.cpp
@@ -6,6 +6,6 @@ extern void showFranciscoName(int x, int y)
     Rect r;
     r.bot = y - 190;
     r.left = x + 280;
-    r.center = 0;
+    r.center = 1;
     ggprint8b(&r, 40, 0x00ffff44, "Francisco Ulloa");
 }
