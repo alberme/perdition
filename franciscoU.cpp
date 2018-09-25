c5e150dda14af823f4737d6c7448715857008e7c
Student <student@lab-240-001.cs.csubak.edu>
2018-09-25 12:17:18 -0700

    changes to walk2.cpp

--- franciscoU.cpp
+++ franciscoU.cpp
@@ -4,8 +4,8 @@
 extern void showFranciscoName(int x, int y)
 {
     Rect r;
-    r.bot = y - 170;
+    r.bot = y - 190;
     r.left = x + 280;
     r.center = 0;
-    ggprint8b(&r, 25, 0x00ffff44, "Francisco U");
+    ggprint8b(&r, 40, 0x00ffff44, "Francisco U");
 }
