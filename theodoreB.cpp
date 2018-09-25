32249f66bbf70ee6fe9abc41f9b7ea1bbc34ce17
Theodore Barcelona <42781037+tbarcelona@users.noreply.github.com>
2018-09-25 11:39:30 -0700

    Add files via upload

--- theodoreB.cpp
+++ theodoreB.cpp
@@ -1,10 +1,15 @@
 //Theodore Barcelona
 //Theo's game source code
+#include <iostream>
+#include "fonts.h"
 
-#include <font.h>
-
-void ShowTheodoreName()
+extern void ShowTheodoreName(int x, int y)
 {
-return("Theodore Barcelona");
+    Rect r;
+    unsigned int c = 0x00ffff44;
+    r.bot = gl.yres - 20;
+    r.left = 10;
+    r.center = 0;
+    ggprint8b(&r, 16, c, "Theodore Barcelona");
 }
 
