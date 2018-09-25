8fde56c56c2e66195ddd97fe301462590e03303b
Student <student@lab-240-002.cs.csubak.edu>
2018-09-25 11:31:10 -0700

    changes to credit file

--- anahiV.cpp
+++ anahiV.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 extern void showAnahiName()
 {
+    Rect r;
+    unsigned int c = 0x00ffff44;
+    r.bot = g.yres - 20;
+    r.left = 10;
+    r.center = 0;
+    ggprint8b(&r, 16, c, "Anahi V");
 
 }
 
