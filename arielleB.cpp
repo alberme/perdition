fa941a79deec721420e6a4525c7cc9697609e772
ArieBattle <42813810+ArieBattle@users.noreply.github.com>
2018-09-25 11:30:54 -0700

    Add files via upload
    arielle source file

--- arielleB.cpp
+++ arielleB.cpp
@@ -1,12 +1,16 @@
 //Arielle Battle
 //Arielle's game source code
 
-#include <font.h>
+#include <iostream>
+#include "font.hi"
 
 
 void ShowArielleName(int x, int y)
 {
-
+	r.bot = gl.yres - 20;
+	r.left = 10;
+	r.center = 0;
+	ggprint8b(&r, 16, c, "Arielle Battle");
 }
 
 
