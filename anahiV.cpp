8a12e74cb25c2f249b7f9017bbe2fc7651b77892
avargas2 <42770684+avargas2@users.noreply.github.com>
2018-09-25 12:21:25 -0700

    Update anahiV.cpp

--- anahiV.cpp
+++ anahiV.cpp
@@ -7,7 +7,7 @@ extern void showAnahiName(int x, int y)
     Rect r;
     r.bot = y - 210;
     r.left = x + 280;
-    r.center = 0;
+    r.center = 1;
     ggprint8b(&r, 40, 0x00ffff44, "Anahi Vargas");
 
 }
