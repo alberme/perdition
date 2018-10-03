31404446165d22332ca62bea6bcab632b62657cd
ArieBattle <42813810+ArieBattle@users.noreply.github.com>
2018-10-03 10:34:19 -0700

    Add files via upload
    name space

--- franciscoU.cpp
+++ franciscoU.cpp
@@ -8,7 +8,7 @@
 extern void showFranciscoName(int x, int y)
 {
     Rect r;
-    r.bot = y - 190;
+    r.bot = y - 200;
     r.left = x + 280;
     r.center = 1;
     ggprint8b(&r, 40, 0x00ffff44, "Francisco Ulloa");
